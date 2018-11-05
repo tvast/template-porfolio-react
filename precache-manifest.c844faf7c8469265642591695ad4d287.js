12471e86f07568f68dc2edfb7fd05ac9b22f5b20
Theophile <theophile@iMac-de-administrador-3.local>
2018-11-05 15:54:12 +0100

    push test github page

---
+++ precache-manifest.c844faf7c8469265642591695ad4d287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6eec6d80624eaeac2ab",
+    "url": "/static/js/main.e6eec6d8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e6eec6d80624eaeac2ab",
+    "url": "/static/css/main.16284270.chunk.css"
+  },
+  {
+    "revision": "21600962a28b45f1f2335fd6366680dc",
+    "url": "/index.html"
+  }
+];
